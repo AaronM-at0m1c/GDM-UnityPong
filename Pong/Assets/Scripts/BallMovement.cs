ac6f0f13adf514ee871559ef18fdacb190313834
Aaron <amccurry209@gmail.com>
2026-01-28 11:45:04 -0500

    Updated BallMovement to add encapsulation, FixedUpdate(), and added comments

--- Pong/Assets/Scripts/BallMovement.cs
+++ Pong/Assets/Scripts/BallMovement.cs
@@ -1,27 +1,56 @@
 using UnityEngine;
 
-public class BallMovement : MonoBehaviour {
+public class BallMovement : MonoBehaviour
+{
 
-    Rigidbody2D rb;
+//Private Fields
+private float speed = 3f;
+private Vector2 direction;
+private Rigidbody2D ball;
 
+//Public Get-Set Properties
+public float Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+
+
+public Vector2 Direction
+    {
+        get { return direction; }
+        set { direction =value.normalized; }
+    }
+
+//Start function
 void Start()
 {
-    rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = new Vector2(3f, 3f);
+    //Create ball object and set starting velocity
+    ball = GetComponent<Rigidbody2D>();
+    direction = new Vector2(1, 1);
+    ball.velocity = direction * speed;
 }
 
-
-
-void update()
-{
-   void OnCollisionEnter2D(Collision2D collision)
-{
-  Rigidbody2D rb = GetComponent<Rigidbody2D>();
-    
-  // Reverse horizontal direction
-  rb.velocity = new Vector2(-rb.velocity.x, rb.velocity.y);
+//Collision Logic
+void OnCollisionEnter2D(Collision2D collision)
+{ 
+  // Reverse horizontal direction for top and bottom walls
+  if (collision.gameObject.name == "Top Wall" || collision.gameObject.name == "Bottom Wall")
+        {
+            direction.y = -direction.y;
+        }
+    //Reverse vertical direction for left and right walls
+    else if (collision.gameObject.name == "Left Wall" || collision.gameObject.name == "Right Wall")
+        {
+            direction.x = -direction.x;
+        }
+  //Debug.Log("Hit: " + collision.gameObject.name); //Sanity check for debugging bounce issue
 }
-}
+
+//Update function to continuously update ball velocity
+void FixedUpdate()
+    {
+        ball.velocity = direction * speed;
+    }
 
 }
-
